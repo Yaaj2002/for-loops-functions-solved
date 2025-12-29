f68452428ee8f7b7e4309c61cae07f8b588ea046
Justin Yang <yangjustin90@gmail.com>
2025-12-29 09:20:52 -0500

    Passed e9

--- exercises/e9.js
+++ exercises/e9.js
@@ -1,15 +1,18 @@
-
 // EXERCISE 9
 // Return an array of bank accounts with balance over $100
 // Array example: bankAccounts in /data/data.js
 // getClientsWithBalanceOverOneHundred(array) => [{ name: 'Name1', balance: 32, ... }, { name: 'Name2', balance: 3523, ... }]
 
 export function getClientsWithBalanceOverOneHundred(array) {
-  // Your code goes here...
+  const results = [];
 
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].balance > 100) {
+      results.push(array[i]);
+    }
+  }
+  return results;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-9"
